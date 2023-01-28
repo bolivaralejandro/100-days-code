4e1fb926a81aa4692fac5b3f38a20a28064f7164
Alejandro Bolivar <abolivar@abolivar-ltm1.internal.salesforce.com>
2023-01-28 08:37:27 -0500

    added laptop files

---
+++ day-45/scrape.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import lxml
+
+with open("website.html") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+# print(soup.title)
+# print(soup.title.name)
+# print(soup.title.string)
+# print(soup.prettify())
+all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+# for tag in all_anchor_tags:
+#     #print(tag.getText())
+#     print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading.name)
+
+# company_url = soup.select_one(selector="p a") #select get all
+# print(company_url)
+# company_url = soup.select_one(selector="#name") #select get all
+# print(company_url)
+company_url = soup.select_one(selector=".heading") #select get all
+print(company_url)
